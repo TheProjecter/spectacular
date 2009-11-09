a6239b810fbbdc9e58a22338fabff5da276226d7
mdowling <mdowling@06af3b2c-cb5f-11de-af9b-21f9cdd2bce9>
2009-11-09 18:51:23 +0000

    Doing some rearranging, adding the appassembler

--- spectacular-core/src/main/java/minderupt/spectacular/spine/SpectacularRunner.java
+++ spectacular-core/src/main/java/minderupt/spectacular/spine/SpectacularRunner.java
@@ -23,15 +23,18 @@ public class SpectacularRunner {
         // Options
         Options options = new Options();
         options.addOption("eucBasePackage", true, "Base Package for Executable Use Case fixtures.");
+        options.addOption("specLocation", true, "Location of the specification to test.");
         options.addOption("config", false, "Beans file that configures Spectacular and wires the spine together");
 
         CommandLineParser cmdLineParse = new PosixParser();
         GlobalOptions globalOptions = new GlobalOptions();
         String springContextFile = "classpath:default-spring-context/*.xml";
+        String specLocation = "";
 
         try {
             CommandLine cmdLine = cmdLineParse.parse(options, args);
             String basePackages = cmdLine.getOptionValue("eucBasePackage");
+            specLocation = cmdLine.getOptionValue("specLocation");
 
             if(cmdLine.hasOption("config")) {
                 springContextFile = cmdLine.getOptionValue("config");
@@ -49,6 +52,7 @@ public class SpectacularRunner {
         ApplicationContext appContext = configureSpine(springContextFile);
         SpectacularSpine spine = (SpectacularSpine) appContext.getBean("spine");
         spine.setGlobalOptions(globalOptions);
+        spine.setSpecificationLocation(specLocation);
         spine.run();
 
 
