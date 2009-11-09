006e141d7d571eed421005f0deebaf9da1778561
mdowling <mdowling@06af3b2c-cb5f-11de-af9b-21f9cdd2bce9>
2009-11-09 18:53:49 +0000

    Doing some rearranging, adding the appassembler

--- trunk/spectacular-core/src/test/java/minderupt/spectacular/parser/html/HTMLTableArtifactExtractorTest.java
+++ trunk/spectacular-core/src/test/java/minderupt/spectacular/parser/html/HTMLTableArtifactExtractorTest.java
@@ -20,7 +20,7 @@ import java.io.File;
  */
 public class HTMLTableArtifactExtractorTest {
 
-    private String baseResourceDir = "src/test/resource";
+    private String baseResourceDir = "src/test/resources";
 
 
     @Before
