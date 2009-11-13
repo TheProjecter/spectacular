f5b6eb577b8c9aae357f7925247834c4e37859dd
mdowling <mdowling@06af3b2c-cb5f-11de-af9b-21f9cdd2bce9>
2009-11-13 19:19:27 +0000

    Start of a JUnit runnable spectacular

---
+++ spectacular-junit/src/test/java/minderupt/spectacular/junit/SpectacularTestCaseTest.java
@@ -0,0 +1,15 @@
+package minderupt.spectacular.junit;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dowlinmi
+ * Date: Nov 13, 2009
+ * Time: 11:03:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SpectacularTestCaseTest {
+
+    
+
+
+}
