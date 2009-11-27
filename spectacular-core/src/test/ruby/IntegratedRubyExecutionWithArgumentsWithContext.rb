4c92fa841750bb99d9919575306980d12b5198d9
mdowling <mdowling@06af3b2c-cb5f-11de-af9b-21f9cdd2bce9>
2009-11-27 05:17:22 +0000

    Added ability to specify output report location in command line.

---
+++ spectacular-core/src/test/ruby/IntegratedRubyExecutionWithArgumentsWithContext.rb
@@ -0,0 +1,12 @@
+Flow "User does ${word} basic" do | word |
+
+  puts "USER DOES SOMETHING BASIC: #{word}"
+  
+end
+
+
+Expectation "User ${userAction} a ${typeOfView} view" do | userAction, typeOfView |
+
+  puts "USER DOES ACTION: #{userAction} / TYPE OF VIEW:  #{typeOfView}"
+  
+end
