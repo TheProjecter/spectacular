3293f8e7f289c7bafd80b59118668f493c97bd32
mdowling <mdowling@06af3b2c-cb5f-11de-af9b-21f9cdd2bce9>
2010-01-05 14:41:38 +0000

    Selenese Variables.

--- trunk/spectacular-core/src/test/java/minderupt/spectacular/executor/euc/selenium/SeleneseExecutableTest.java
+++ trunk/spectacular-core/src/test/java/minderupt/spectacular/executor/euc/selenium/SeleneseExecutableTest.java
@@ -50,6 +50,44 @@ public class SeleneseExecutableTest {
 
 
     @Test
+    public void testExecuteFlowStepWithSingleArgument() throws Exception {
+
+        // set up selenese part
+        List<String> commandOpen = new LinkedList<String>();
+        commandOpen.add("open");
+        commandOpen.add("/some/url");
+
+        List<String> commandClick = new LinkedList<String>();
+        commandClick.add("click");
+        commandClick.add("id=$0");
+
+        SelenesePart part = new SelenesePart();
+        part.setStep("User wants to do (.*) useful");
+        part.getCommands().add(commandOpen);
+        part.getCommands().add(commandClick);
+
+        // mock out selenium
+        SpectacularSelenium selenium = mock(SpectacularSelenium.class);
+        SeleneseExecutable exec = new SeleneseExecutable(part);
+        exec.setSeleniumInstance(selenium);
+
+        // set up arguments from
+        List<String> args = new LinkedList<String>();
+        args.add("argButtonName"); // as if the step said "User wants to do argButtonName useful"
+
+        exec.executeSelenese(new Context(), args);
+
+        verify(selenium).executeSeleneseCommand("open", new String[]{"/some/url"});
+        verify(selenium).executeSeleneseCommand("click", new String[]{"id=argButtonName"});
+
+
+    }
+
+
+
+
+
+    @Test
     public void testGetExecutableMethod() throws Exception {
 
         SeleneseExecutable exec = new SeleneseExecutable(null);
