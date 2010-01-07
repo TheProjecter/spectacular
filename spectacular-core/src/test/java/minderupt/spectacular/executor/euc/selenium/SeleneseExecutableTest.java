531356766f27afdba03b7f442bb05c180123e423
mdowling <mdowling@06af3b2c-cb5f-11de-af9b-21f9cdd2bce9>
2010-01-07 16:38:24 +0000

    Defects around selenese variables.

--- spectacular-core/src/test/java/minderupt/spectacular/executor/euc/selenium/SeleneseExecutableTest.java
+++ spectacular-core/src/test/java/minderupt/spectacular/executor/euc/selenium/SeleneseExecutableTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.verify;
  */
 public class SeleneseExecutableTest {
 
-    
+
     @Test
     public void testSimpleFlowStep() throws Exception {
 
@@ -85,7 +85,55 @@ public class SeleneseExecutableTest {
     }
 
 
+    @Test
+    public void testExecuteFlowStepWithMultipleArgument() throws Exception {
 
+        // set up selenese part
+        List<String> commandOpen = new LinkedList<String>();
+        commandOpen.add("open");
+        commandOpen.add("/some/url");
+
+        List<String> commandClickZero = new LinkedList<String>();
+        commandClickZero.add("click");
+        commandClickZero.add("idA=$0");
+
+        List<String> commandClickOne = new LinkedList<String>();
+        commandClickOne.add("click");
+        commandClickOne.add("idB=$1");
+
+        List<String> commandClickTwo = new LinkedList<String>();
+        commandClickTwo.add("click");
+        commandClickTwo.add("idC=$2");
+
+        SelenesePart part = new SelenesePart();
+        part.setStep("User wants to do (.*) useful with some (.*) of (.*) with apples");
+        part.getCommands().add(commandOpen);
+        part.getCommands().add(commandClickZero);
+        part.getCommands().add(commandClickOne);
+        part.getCommands().add(commandClickTwo);
+
+        // mock out selenium
+        SpectacularSelenium selenium = mock(SpectacularSelenium.class);
+        SeleneseExecutable exec = new SeleneseExecutable(part);
+        exec.setSeleniumInstance(selenium);
+
+        // set up arguments from
+        List<String> args = new LinkedList<String>();
+
+        // as if the step said "User wants to do argButtonNameZero useful with some argButtonNameOne of argButtonNameTwo with apples"
+        args.add("argButtonNameZero");
+        args.add("argButtonNameOne");
+        args.add("argButtonNameTwo");
+
+        exec.executeSelenese(new Context(), args.toArray());
+
+        verify(selenium).executeSeleneseCommand("open", new String[]{"/some/url"});
+        verify(selenium).executeSeleneseCommand("click", new String[]{"idA=argButtonNameZero"});
+        verify(selenium).executeSeleneseCommand("click", new String[]{"idB=argButtonNameOne"});
+        verify(selenium).executeSeleneseCommand("click", new String[]{"idC=argButtonNameTwo"});
+
+
+    }
 
 
     @Test
